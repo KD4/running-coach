2199a548f2b2ca1100444c597cd143fd0b6db7df
Kang DDan DDan <kangddanddan@gmail.com>
2026-02-20 19:17:55 +0900

    second

---
+++ backend/src/main/kotlin/com/kd4/runningcoach/RunningCoachApplication.kt
@@ -0,0 +1,11 @@
+package com.kd4.runningcoach
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RunningCoachApplication
+
+fun main(args: Array<String>) {
+    runApplication<RunningCoachApplication>(*args)
+}
